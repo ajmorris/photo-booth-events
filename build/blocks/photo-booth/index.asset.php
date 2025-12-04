f3d153a44e57aa90dc2c4ae6e9a9e2229971667f
AJ Morris <aj@ajmorris.me>
2025-12-03 19:36:52 -0500

    Adding Build files for distribution

---
+++ build/blocks/photo-booth/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '3d0453ffa4af9bf438db');
